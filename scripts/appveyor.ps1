86fa327ec8465f098ee4d8b93d94b4610153daf9
Marie Maurer <martin.github-new-20161117.maurer@clibb.de>
2019-10-15 07:18:20 +0200

    Update appveyor.ps1

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -8,6 +8,6 @@ function Unzip
 }
 
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile("http://bit.ly/1JPHkL3", "C:\projects\cryptominisat\boost_1_59_0.zip")
+$wc.DownloadFile("https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.zip", "C:\projects\cryptominisat\boost_1_71_0.zip")
 
-Unzip "C:\projects\cryptominisat\boost_1_59_0.zip" "C:\projects\cryptominisat"
+Unzip "C:\projects\cryptominisat\boost_1_71_0.zip" "C:\projects\cryptominisat"
